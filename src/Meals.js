c532c58eb0e6d010aad6a5eefd1d8b9f3bdac533
Elvina <149134113+elvina-s@users.noreply.github.com>
2024-02-21 18:20:30 +0100

    Update Meals.js

--- src/Meals.js
+++ src/Meals.js
@@ -1,12 +1,17 @@
 import { FiEdit3 } from "react-icons/fi";
 import { MdOutlineDelete } from "react-icons/md";
+import "./index.css";
 
 const Meals = ({ text, updatingInInput, deleteMeal }) => {
     return (
-        <div>
-            <p>{text}</p>
-            <FiEdit3 onClick={updatingInInput}/>
-            <MdOutlineDelete onClick={deleteMeal}/>
+        <div className="Meals">
+            <div className="aMeal">
+                <p>{text}</p>
+                <div className="buttons">
+                    <FiEdit3 onClick={updatingInInput} className="icon first" />
+                    <MdOutlineDelete onClick={deleteMeal} className="icon second" />
+                </div>
+            </div>
         </div>
     );
 }
